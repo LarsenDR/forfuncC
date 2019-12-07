f100b9f64ca4abe814602df2b3ad19009620754b
Dave Larsen <larsendr@missouri.edu>
2019-12-07 11:57:42 -0600

    cleaning up my c code

--- C/basalarea.c
+++ C/basalarea.c
@@ -1,21 +1,24 @@
-
+#include <stdlib.h>
+#include <string.h>
 #include "forfuncC.h"
 
-int arlen = 7;
+int arrayLength = 7;
 
-float basalarea(float dia[], float wt[], char unittype[]) {
+// Basalarea function to basalarea per unit area for a list of trees.
+float basalarea(float dia[], float wt[], char* unittype) {
 	float cst, ba;
 	cst = 0.005454154;
-	if (unittype == "metric") {
+	if (!strcmp(unittype, "metric")) {
 		cst = 0.00007856;
 	}
 
-	for( int i = 0; i < arlen; i++ ){
+	for( int i = 0; i < arrayLength; i++ ){
 		ba = ba + cst*dia[i]*dia[i]*wt[i];
 	}
 	return( ba );
 }
 
+//Test main function
 void main() {
 	float dia[] = {8, 6, 8, 5, 4, 6, 7};
 	float wt[] = {10, 10, 10, 10, 10, 10, 10};
